ed4737e969bb9078168b4ef9680628ef23d4bd66
Arlen Li <329603359@qq.com>
2020-08-04 19:40:01 +1000

    Add MinimalTree

---
+++ TreesAndGraphs/BinaryTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreesAndGraphs
+{
+    public class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ TreesAndGraphs/MinimalTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreesAndGraphs
+{
+    public class MinimalTree
+    {
+        public static BinaryTreeNode<int> MinimalTreeAlgo (int[] array)
+        {
+            return MinimalTreeAlgo(array, 0, array.Length - 1);
+        }
+
+        private static BinaryTreeNode<int> MinimalTreeAlgo(int[] array, int startingIndex, int endingIndex)
+        {
+            if (startingIndex > endingIndex)
+                return null;
+            var midIndex = (startingIndex + endingIndex) / 2;
+            var node = new BinaryTreeNode<int>(array[midIndex]);
+            node.Left = MinimalTreeAlgo(array, startingIndex, midIndex - 1);
+            node.Right = MinimalTreeAlgo(array, midIndex + 1, endingIndex);
+            return node;
+        }
+    }
+}
