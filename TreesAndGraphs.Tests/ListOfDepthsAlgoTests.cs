f9e808fbfa06471065313854e99b41dd9c11604d
Arlen Li <329603359@qq.com>
2020-08-04 20:23:34 +1000

    Add Algo and tests for List of Depths

---
+++ TreesAndGraphs.Tests/ListOfDepthsAlgoTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TreesAndGraphs.Tests
+{
+    public class ListOfDepthsAlgoTests
+    {
+        [Test]
+        public void ListOfDepthsAlgo_WhenRootIsNull_ReturnsEmptyLists()
+        {
+            // Arrange & Act
+            var results = ListOfDepths.ListOfDepthsAlgo(null);
+
+            // Assert
+            results.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void ListOfDepthsAlgo_WhenRootIsNotNull_ReturnsCorrectLists()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(10);
+            var rootLeft = new BinaryTreeNode<int>(4);
+            var rootRight = new BinaryTreeNode<int>(4);
+            root.Left = rootLeft;
+            root.Right = rootRight;
+            var rootLeftLeft = new BinaryTreeNode<int>(2);
+            var rootLeftRight = new BinaryTreeNode<int>(8);
+            rootLeft.Left = rootLeftLeft;
+            rootLeft.Right = rootLeftRight;
+            var rootRightRight = new BinaryTreeNode<int>(20);
+            rootRight.Right = rootRightRight;
+
+            // Act
+            var results = ListOfDepths.ListOfDepthsAlgo(root);
+
+            // Assert
+            results.Count.Should().Be(3);
+            results[0].Count.Should().Be(1);
+            results[1].Count.Should().Be(2);
+            results[2].Count.Should().Be(3);
+        }
+    }
+}
---
+++ TreesAndGraphs/ListOfDepths.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace TreesAndGraphs
+{
+    public class ListOfDepths
+    {
+        public static List<List<BinaryTreeNode<int>>> ListOfDepthsAlgo(BinaryTreeNode<int> root)
+        {
+            var results = new List<List<BinaryTreeNode<int>>>();
+            if (root == null)
+                return results;
+            var currectDepthList = new List<BinaryTreeNode<int>>();
+            var nextDepthList = new List<BinaryTreeNode<int>>();
+            currectDepthList.Add(root);
+            while(currectDepthList.Count != 0)
+            {
+                results.Add(currectDepthList);
+                nextDepthList = new List<BinaryTreeNode<int>>();
+                foreach(var node in currectDepthList)
+                {
+                    if (node.Left != null)
+                        nextDepthList.Add(node.Left);
+                    if (node.Right != null)
+                        nextDepthList.Add(node.Right);
+                }
+                currectDepthList = nextDepthList;
+            }
+            return results;
+        }
+    }
+}
