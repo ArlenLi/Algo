c5621e0484aa6c559ca8ec978c181855825e1a0e
Arlen Li <329603359@qq.com>
2020-08-20 22:56:39 +1000

    Add BSTSequences Algo and tests

---
+++ TreesAndGraphs.Tests/BSTSequencesTests.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace TreesAndGraphs.Tests
+{
+    public class BSTSequencesTests
+    {
+        [Test]
+        public void BSTSequencesAlgo_WhenRootIsNull_ReturnEmptyLists()
+        {
+            // Act
+            var actual = BSTSequences.BSTSequencesAlog(null);
+
+            // Assert
+            actual.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void BSTSequencesAlgo_WhenTreeIsMini_ReturnCorrectLists()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(2);
+            root.Left = new BinaryTreeNode<int>(1);
+            root.Right = new BinaryTreeNode<int>(3);
+            // Act
+            var actual = BSTSequences.BSTSequencesAlog(root);
+
+            // Assert
+            actual.Count.Should().Be(2);
+            actual[0][0].Should().Be(2);
+            actual[1][0].Should().Be(2);
+        }
+
+        [Test]
+        public void BSTSequencesAlgo_WhenTreeIsSmall_ReturnCorrectLists()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(2);
+            root.Left = new BinaryTreeNode<int>(1);
+            root.Right = new BinaryTreeNode<int>(4);
+            root.Right.Left = new BinaryTreeNode<int>(3);
+
+            // Act
+            var actual = BSTSequences.BSTSequencesAlog(root);
+
+            // Assert
+            actual.Count.Should().Be(3);
+            actual[0][0].Should().Be(2);
+            actual[1][0].Should().Be(2);
+            actual[2][0].Should().Be(2);
+        }
+
+        [Test]
+        public void BSTSequencesAlgo_WhenTreeIsMedium_ReturnCorrectLists()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(10);
+            root.Left = new BinaryTreeNode<int>(5);
+            root.Left.Left = new BinaryTreeNode<int>(1);
+            root.Right = new BinaryTreeNode<int>(15);
+            root.Right.Left = new BinaryTreeNode<int>(12);
+            root.Right.Right = new BinaryTreeNode<int>(19);
+
+
+            // Act
+            var actual = BSTSequences.BSTSequencesAlog(root);
+
+            // Assert
+            foreach (var eachList in actual)
+            {
+                var output = "";
+                foreach (var num in eachList)
+                {
+                    output += num;
+                    output += " ";
+                }
+                Debug.WriteLine(output);
+            }
+            actual.Count.Should().Be(20);
+        }
+    }
+}
---
+++ TreesAndGraphs/BSTSequences.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TreesAndGraphs
+{
+    /*
+     * BST Sequences: A binary search tree was created by traversing through an array from left to right
+     * and inserting each element. Given a binary search tree with distinct elements, print all possible
+     * arrays that could have led to this tree.
+     */
+    public class BSTSequences
+    {
+        public static List<List<int>> BSTSequencesAlog(BinaryTreeNode<int> node)
+        {
+            if(node == null)
+                return new List<List<int>>();
+            var prefixSequence = new List<int> { node.Data };
+            var leftSequences = BSTSequencesAlog(node.Left);
+            var rightSequences = BSTSequencesAlog(node.Right);
+
+            var result = new List<List<int>>();
+            if (leftSequences.Count == 0 && rightSequences.Count == 0)
+                result.Add(prefixSequence);
+            else if (leftSequences.Count == 0)
+            {
+                foreach (var rightSequence in rightSequences)
+                        Weave(new List<int>(), rightSequence, prefixSequence, result);
+            }
+            else if (rightSequences.Count == 0)
+            {
+                foreach (var leftSequence in leftSequences)
+                    Weave(leftSequence, new List<int>(), prefixSequence, result);
+            }
+            else
+            {
+                foreach (var leftSequence in leftSequences)
+                    foreach (var rightSequence in rightSequences)
+                        Weave(leftSequence, rightSequence, prefixSequence, result);
+            }
+            return result;
+        }
+
+        private static void Weave(List<int> leftSequence, List<int> rightSequence, List<int> prefixSequence, List<List<int>> result)
+        {
+            if (leftSequence.Count == 0 || rightSequence.Count == 0)
+            {
+                result.Add(prefixSequence.Concat(leftSequence).Concat(rightSequence).ToList());
+                return;
+            }
+
+            var leftHead = leftSequence[0];
+            leftSequence.RemoveAt(0);
+            prefixSequence.Add(leftHead);
+            Weave(leftSequence, rightSequence, prefixSequence, result);
+            prefixSequence.RemoveAt(prefixSequence.Count - 1);
+            leftSequence.Insert(0, leftHead);
+
+            var rightHead = rightSequence[0];
+            rightSequence.RemoveAt(0);
+            prefixSequence.Add(rightHead);
+            Weave(leftSequence, rightSequence, prefixSequence, result);
+            prefixSequence.RemoveAt(prefixSequence.Count - 1);
+            rightSequence.Insert(0, rightHead);
+        }
+    }
+}
