e15cff890452348d89d0e51dc8c30a50d2c44e05
Arlen Li <329603359@qq.com>
2020-08-29 11:59:04 +1000

    Add PathWithSum Algo and Tests

---
+++ TreesAndGraphs.Tests/PathWIthSumTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using FluentAssertions.Specialized;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreesAndGraphs.Tests
+{
+    public class PathWithSumTests
+    {
+        [Test]
+        public void PathWithSumTests_GivenATreeNode_GivenNumIs0_ReturnsCorrectResult()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(3);
+            var rootLeft = new BinaryTreeNode<int>(7);
+            var rootRight = new BinaryTreeNode<int>(10);
+            root.Left = rootLeft;
+            root.Right = rootRight;
+            var rootLeftLeft = new BinaryTreeNode<int>(-10);
+            var rootLeftRight = new BinaryTreeNode<int>(6);
+            rootLeft.Left = rootLeftLeft;
+            rootLeft.Right = rootLeftRight;
+            var rootLeftLeftLeft = new BinaryTreeNode<int>(10);
+            rootLeftLeft.Left = rootLeftLeftLeft;
+            var rootRightLeft = new BinaryTreeNode<int>(7);
+            var rootRightRight = new BinaryTreeNode<int>(-13);
+            rootRight.Left = rootRightLeft;
+            rootRight.Right = rootRightRight;
+
+            // Act
+            var actual = PathWithSum.PathWithSumAlgo(root, 0);
+
+            // Assert
+            actual.Should().Be(3);
+        }
+
+        [Test]
+        public void PathWithSumTests_GivenATreeNode_GiveNumIs10_ReturnsCorrectResult()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(3);
+            var rootLeft = new BinaryTreeNode<int>(7);
+            var rootRight = new BinaryTreeNode<int>(10);
+            root.Left = rootLeft;
+            root.Right = rootRight;
+            var rootLeftLeft = new BinaryTreeNode<int>(-10);
+            var rootLeftRight = new BinaryTreeNode<int>(6);
+            rootLeft.Left = rootLeftLeft;
+            rootLeft.Right = rootLeftRight;
+            var rootLeftLeftLeft = new BinaryTreeNode<int>(10);
+            rootLeftLeft.Left = rootLeftLeftLeft;
+            var rootRightLeft = new BinaryTreeNode<int>(7);
+            var rootRightRight = new BinaryTreeNode<int>(-13);
+            rootRight.Left = rootRightLeft;
+            rootRight.Right = rootRightRight;
+
+            // Act
+            var actual = PathWithSum.PathWithSumAlgo(root, 10);
+
+            // Assert
+            actual.Should().Be(4);
+        }
+    }
+}
---
+++ TreesAndGraphs/PathWithSum.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace TreesAndGraphs
+{
+    /*Paths with Sum: You are given a binary tree in which each node contains an integer value (which
+     *might be positive or negative). Design an algorithm to count the number of paths that sum to a
+     *given value. The path does not need to start or end at the root or a leaf, but it must go downwards
+     *(traveling only from parent nodes to child nodes).
+     */
+    public class PathWithSum
+    {
+        public static int PathWithSumAlgo(BinaryTreeNode<int> node, int givenNum)
+        {
+            return PathWithSumAlgo(node, givenNum, 0, new Hashtable());
+        }
+
+        private static int PathWithSumAlgo(BinaryTreeNode<int> node, int givenNum, int currentSum, Hashtable hashtable)
+        {
+            if (node == null)
+                return 0;
+            var counter = 0;
+            currentSum += node.Data;
+            var runningSum = currentSum - givenNum;
+            if(hashtable.ContainsKey(runningSum))
+            {
+                counter += (int)hashtable[runningSum];
+            }
+
+            if (currentSum == givenNum)
+                counter++;
+
+            if (hashtable.ContainsKey(currentSum))
+                hashtable[currentSum] = (int)hashtable[currentSum] + 1;
+            else
+                hashtable[currentSum] = 1;
+
+            var leftCounter = PathWithSumAlgo(node.Left, givenNum, currentSum, hashtable);
+            var rightCounter = PathWithSumAlgo(node.Right, givenNum, currentSum, hashtable);
+
+            if ((int)hashtable[currentSum] == 1)
+                hashtable.Remove(currentSum);
+            else
+                hashtable[currentSum] = (int)hashtable[currentSum] - 1;
+
+            return counter + leftCounter + rightCounter;
+        }
+    }
+}
