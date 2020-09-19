89d8a6383a2dec448cf0e1a723240aee2c0e81af
Arlen Li <329603359@qq.com>
2020-09-19 23:25:46 +1000

    Add PowerSet Algo and Tests

---
+++ Dynamic.Tests/PowerSetTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamic.Tests
+{
+    public class PowerSetTests
+    {
+        [Test]
+        public void PowerSetAlgo_WhenSetIsEmpty_SubsetsShouldOnlyCotainsOneEmptySet() 
+        {
+            // Act
+            var actual = PowerSet<char>.PowerSetAlgo(new HashSet<char>());
+
+            // Assert
+            actual.Count.Should().Be(1);
+            actual[0].Count.Should().Be(0);
+        }
+
+        [Test]
+        public void PowerSetAlgo_WhenSetIsNotEmpty_ReturnCorrectSubsets()
+        {
+            // Arrange
+            var expected = new List<HashSet<char>>();
+            expected.Add(new HashSet<char>());
+            expected.Add(new HashSet<char>() {'a'});
+            expected.Add(new HashSet<char>() {'b'});
+            expected.Add(new HashSet<char>() {'c'});
+            expected.Add(new HashSet<char>() {'a', 'b'});
+            expected.Add(new HashSet<char>() {'a', 'c'});
+            expected.Add(new HashSet<char>() {'b', 'c'});
+            expected.Add(new HashSet<char>() {'a', 'b', 'c'});
+            
+            // Act
+            var actual = PowerSet<char>.PowerSetAlgo(new HashSet<char>(new char[]{ 'a', 'b', 'c'}));
+
+            // Assert
+            actual.Count.Should().Be(8);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ Dynamic/PowerSet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Dynamic
+{
+    /*
+     * Power Set: Write a method to return all subsets of a set.
+     * Tip: How to generate subsets of {a, b, c} from subsets of {a, b}
+     */
+    public class PowerSet<T>
+    {
+        public static List<HashSet<T>> PowerSetAlgo(HashSet<T> set)
+        {
+            var subsets = new List<HashSet<T>>();
+            if(set.Count == 0)
+            {
+                subsets.Add(new HashSet<T>());
+                return subsets;
+            }
+
+            var enumerator = set.GetEnumerator();
+            enumerator.MoveNext();
+            var removedElement = enumerator.Current;
+            set.Remove(removedElement);
+            var subsetsOfOneElementRemoved = PowerSetAlgo(set);
+            foreach(var subset in subsetsOfOneElementRemoved)
+            {
+                subsets.Add(subset);
+                var newSubset = new HashSet<T>(subset);
+                newSubset.Add(removedElement);
+                subsets.Add(newSubset);
+            }
+            return subsets;
+        }
+    }
+}
